24a65233b278c46efd2ee597144e324b43ee77f7
BalvesD3v <lpbalves7filho@gmail.com>
2024-09-12 18:23:36 -0300

    feat: model service created

---
+++ src/models/Service.ts
@@ -0,0 +1,35 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IService extends Document {
+    description: string
+    serviceDate: Date
+    vehicleId: mongoose.Types.ObjectId
+    clientId: mongoose.Types.ObjectId
+    status: "pending" | "completed" | "cancelled"
+    price: number
+}
+
+const ServiceSchema: Schema = new Schema({
+   description: {
+    type: String,
+    required: true
+   },
+   serviceDate: {
+    type: Date,
+    required: true
+   },
+   vehicleId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+   },
+   status: {
+    type: String,
+    required: true
+   },
+   price: {
+    type: Number,
+    required: true
+   }
+})
+
+export default mongoose.model<IService>("Service", ServiceSchema)
