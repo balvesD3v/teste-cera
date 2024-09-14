9811102b34db679bc0f22fc5ba2b548c6ed00117
BalvesD3v <lpbalves7filho@gmail.com>
2024-09-14 19:10:47 -0300

    chore: zod validation'

--- src/validators/service.validators.ts
+++ src/validators/service.validators.ts
@@ -15,11 +15,16 @@ const objectIdSchema = z
   .transform((value) => new Types.ObjectId(value))
 
 export const serviceSchema = z.object({
-  description: z.string().optional(),
-  serviceDate: z.string().optional().transform(dateStringToDate),
+  description: z.string().min(1, 'Descrição é obrigatória'),
+  serviceDate: z
+    .string()
+    .refine((date) => !isNaN(Date.parse(date)), {
+      message: 'Data inválida',
+    })
+    .transform(dateStringToDate),
   vehicleId: objectIdSchema,
   clientId: objectIdSchema,
-  status: z.enum(['pending', 'completed', 'canceled']).optional(),
+  status: z.enum(['pending', 'completed', 'canceled']),
   price: z.number().positive('Preço deve ser um valor positivo'),
 })
 
