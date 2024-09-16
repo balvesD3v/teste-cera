81d36f36968f8cafc9d2ef50f683f15bc7a9faf9
BalvesD3v <lpbalves7filho@gmail.com>
2024-09-15 21:29:55 -0300

    feat: env.ts create

--- src/infra/app.ts
+++ src/infra/app.ts
@@ -1,15 +1,19 @@
 import express from 'express'
 import serviceRoutes from './routes/service.routes'
 import mongoose from 'mongoose'
+import 'dotenv/config'
+import { env } from './env/env'
 
 const app = express()
 
 app.use(express.json())
 
+console.log(process.env)
+
 app.use('/api', serviceRoutes)
 
-const mongoUri =
-  'mongodb+srv://luiz:lp15102002@cluster0.qkm6n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const mongoUri = env.DATABASE_URL
+
 mongoose
   .connect(mongoUri, {})
   .then(() => console.log('Conectado ao mongo DB'))
